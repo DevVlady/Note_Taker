f58a8cffe0570b49ca4ab69f9c58c3ad58ad140a
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-11-27 13:11:19 -0600

    Added the routes and data folder.

---
+++ data/noteData.js
@@ -0,0 +1,13 @@
+//Data
+//The array below will hold the data taken from the notepad input
+//The array below is listed with empty string
+
+var notepadArray = [
+    {
+        noteTitle: '',
+        noteText: '',
+    }
+];
+
+//Note will export the array and make it accessible to other files
+module.exports = notepadArray;
---
+++ data/noteList.js
@@ -0,0 +1,12 @@
+//Data
+//The array below will hold the data taken from the notepad input
+//The array below is listed with empty string
+
+var noteListArray = [
+    {
+        noteTitle: '',
+    }
+];
+
+//Note will export the array and make it accessible to other files
+module.exports = noteListArray;
