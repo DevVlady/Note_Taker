7f1564b1bf3a113e8c27b97717adf5cc335dc5b9
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-11-27 13:39:22 -0600

    Added the functions for the post and get method for the apiRoutes js file.

---
+++ routes/apiRoutes.js
@@ -0,0 +1,33 @@
+//Load data
+//Linking route to data sources which hold the arrays
+var noteData = require("..data/noteData");
+var noteList = require("..data/noteList");
+
+//Routing info
+module.exports = function(app) {
+    //API GET method request
+
+    //Get method for the notes inside the list
+    app.get("/api/notes", function(req, res) {
+        res.json(noteData);
+    })
+
+    //Get mothod for the note lists
+    app.get("/api/lists", function(req, res) {
+        res.json(noteList);
+    })
+
+
+    //API POST Method
+    app.post("/api/notes", function(req, res) {
+        if (noteData) {
+            noteData.push(req.body);
+            res.json(true);
+        } else {
+            res.json(false);
+        }
+    });
+
+    //Write code to delete lists when the delete button is pressed
+    
+};
