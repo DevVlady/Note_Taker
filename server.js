147f6b2551e8f1275ccaf31f2070d24d42273c19
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-11-27 14:11:16 -0600

    Successfully added my server info and tested this to ensure my info is displayed when i pull up the server in the url.

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('')
+    res.send('Blah Blah Blah')
 })
 
-app.listen(3000);
+app.listen(3005);
+
+//Parse application
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Parse application/json
+app.use(bodyParser.json());
+
+app.use(function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('you posted:\n');
+    res.end(JSON.stringify(req.body, null, 2))
+})
