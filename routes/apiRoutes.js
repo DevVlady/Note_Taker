80f214c5dc5d4b16be18f9ca075cab52ffa651b4
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-12-04 21:31:16 -0600

    Working on my delete method whcih isnt working.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -25,8 +25,8 @@ router.delete("/notes:id", (req, res) => {
     console.log('**DELETE_METHOD**')
     // Use removeNote method in the store object
     store
-        .removeNote(req.params.id)
-        .then((note) => res.json(note))
+        .removeNote(req.params.id - 1)
+        .then((notes) => res.json(notes))
         .catch((err) => res.status(500).json(err));
 })
 
@@ -37,52 +37,6 @@ module.exports = router;
 
 
 
-
-
-
-
-
-
-
-//Linking route to data sources which hold the arrays
-// const router = require("express").Router();
-// const noteData = require('../data/db.json');
-// const fs = require('fs');
-// const util = require('util');
-// const writeFileAsync = util.promisify(fs.writeFile);
-
-
-// module.exports = function (router) {
-//     // GET "/api/notes" responds with all notes from the database
-//     router.get("/api/notes", function (req, res) {
-//         console.log('**GET_METHOD**')
-//         fs.readFile("./data/db.json", function (err, data) {
-//             console.log(noteData)
-//             //Throw err code if err
-//             if (err) throw err;
-//             let allNotes = JSON.parse(data);
-//             return res.json(allNotes);
-//         })
-//     })
-
-    // POST "/api/notes"
-    // router.post("/api/notes", function (req, res) {
-    //     console.log('**POST_METHOD**')
-    //     const {body: {title, text}} = req;
-
-    //     let newNote = {
-    //         "id": noteData.length + 1,
-    //         "title": title,
-    //         "text": text
-    //     }
-    //     noteData.push(newNote);
-    //     res.json(noteData);
-
-    //     writeFileAsync("./data/db.json", JSON.stringify(noteData), function(err) {
-    //         if(err) throw err;
-    //     })
-
-    // })
 
     // DELETE "/api/notes" deletes the note with an id equal to req.params.id
 //     router.delete("/api/notes/:id", function (req, res) {
@@ -110,4 +64,3 @@ module.exports = router;
 //             });
 //         });
 //     });
-// }
--- server.js
+++ server.js
@@ -18,18 +18,3 @@ app.use("/", htmlRoutes);
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 });
-
-
-// const express = require('express');
-
-// Initialize the app and create a port
-// const app = express();
-// const PORT = process.env.PORT || 8080;
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static('public'));
-
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
-
